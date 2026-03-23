a50dca9c18a909b780e168279fc51c2e4da9e90b
Nicholas Huffman <njh102304@gmail.com>
2026-03-23 15:42:17 -0400

    fullClientServer

--- Client.java
+++ Client.java
@@ -5,14 +5,12 @@ import java.net.InetAddress;
 import java.util.Scanner;
 
 public class Client {
-    private static boolean sending = true;
-
     public static void main(String args[]) throws IOException {
         // takes input from command line for datagram
         Scanner sc = new Scanner(System.in);
 
         // socket to send and recieve datagrams
-        DatagramSocket socket = new DatagramSocket();
+        DatagramSocket socket = new DatagramSocket(8081);
 
         // local ip address
         InetAddress ipAddress = InetAddress.getLocalHost();
@@ -20,10 +18,17 @@ public class Client {
         // byte buffer for datagram creation
         byte[] buffer = new byte[65535];
 
+        // Client flags
+        boolean sending = true;
+        boolean quiting = false;
+
         // client runs until user enters "quit"
         // client swaps mode after sending
         while (true) {
             while (sending) {
+                // clear buffer
+                buffer = new byte[65535];
+
                 // collect datagram data from command line
                 String input = sc.nextLine();
                 buffer = input.getBytes();
@@ -36,15 +41,19 @@ public class Client {
 
                 // quit on user command
                 if (input.equals("quit") || input.equals("q")) {
+                    quiting = true;
                     break;
                 }
 
-                // swap to recieving
+                // swap mode
                 sending = false;
             }
 
             // client swaps mode after recieving [or timeout]
             while (!sending) {
+                // clear buffer
+                buffer = new byte[65535];
+
                 // create datagram shell
                 DatagramPacket datagram = new DatagramPacket(buffer, buffer.length);
 
@@ -58,15 +67,16 @@ public class Client {
                 if (byteToString(buffer).equals("quit")
                         || byteToString(buffer).equals("q")) {
                     System.out.println("EXITING...");
+                    quiting = true;
                     break;
                 }
 
-                // clear buffer and swap mode
-                buffer = new byte[65535];
+                // swap mode
                 sending = true;
             }
             // end complete loop if broken.
-            break;
+            if (quiting)
+                break;
         }
         sc.close();
         socket.close();
@@ -79,7 +89,7 @@ public class Client {
         StringBuilder stringBuilder = new StringBuilder();
         // iterates through bytes, casting into characters
         // stops when hits empty byte
-        for (int i = 0; bytes[i] != 0; i++) {
+        for (int i = 0; i < bytes.length; i++) {
             stringBuilder.append((char) bytes[i]);
         }
         return stringBuilder.toString();
--- Server.java
+++ Server.java
@@ -1,38 +1,70 @@
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 public class Server {
     public static void main(String[] args) throws IOException {
-        // socket to listen for datagrams
+        // socket to send and recieve datagrams
         DatagramSocket socket = new DatagramSocket(8080);
 
         // recieve buffer
         byte[] buffer = new byte[65535];
 
+        // local ip address
+        InetAddress ipAddress = InetAddress.getLocalHost();
+
+        // Server flags
+        boolean sending = false;
+        boolean quiting = false;
+
         // server runs until client sends quit
         while (true) {
-            // create datagram shell
-            DatagramPacket datagram = new DatagramPacket(buffer, buffer.length);
+            while (!sending) {
+                // clear buffer
+                buffer = new byte[65535];
 
-            // recive the datagram
-            socket.receive(datagram);
+                // create datagram shell
+                DatagramPacket datagram = new DatagramPacket(buffer, buffer.length);
 
-            // show recieved data
-            System.out.println("Client sent: " + byteToString(buffer));
+                // recive the datagram
+                socket.receive(datagram);
 
-            // quit if client quit
-            if (byteToString(buffer).equals("quit")
-                    || byteToString(buffer).equals("q")) {
-                System.out.println("EXITING...");
-                break;
+                // show recieved data
+                System.out.println("Client sent: " + byteToString(buffer));
+
+                // quit if client quit
+                if (byteToString(buffer).equals("quit")
+                        || byteToString(buffer).equals("q")) {
+                    System.out.println("EXITING...");
+                    quiting = true;
+                    break;
+                }
+
+                // swap mode
+                sending = true;
             }
+            while (sending) {
+                // clear buffer
+                buffer = new byte[65535];
 
-            // clear buffer after each message
-            buffer = new byte[65535];
+                // send ACK
+                // create dummy imput for testing
+                String dummyInput = "Testing";
+                buffer = dummyInput.getBytes();
 
-            // send ACK
+                // create datagram
+                DatagramPacket datagram = new DatagramPacket(buffer, buffer.length, ipAddress, 8081);
 
+                // send datagram
+                socket.send(datagram);
+
+                // swap mode
+                sending = false;
+            }
+            // end complete loop if broken.
+            if (quiting)
+                break;
         }
         socket.close();
     }
